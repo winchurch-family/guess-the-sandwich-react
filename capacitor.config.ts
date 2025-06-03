a7cc70fb23801c0f8c3d9f646a3aa4adf5927f3f
David Winchurch <davidwinchurch@gmail.com>
2025-06-03 20:52:28 +0100

    Add iOS support using Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.winchurchfamily.guessthesandwich',
+  appName: 'Guess the Sandwich',
+  webDir: 'dist'
+};
+
+export default config;
